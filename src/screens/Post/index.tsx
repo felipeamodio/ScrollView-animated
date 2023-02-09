e87aba3c0a7a11938af0863918dd028a9d039534
Felipe Alves <felipeaamodio@gmail.com>
2023-02-08 21:21:10 -0300

    first commit

---
+++ src/screens/Post/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as S from './styles';
+
+export function Post(){
+    return(
+        <S.Container>
+            <S.Title>Lorem ipsum</S.Title>
+        </S.Container>
+    )
+}
